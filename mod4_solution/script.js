96a4425d00483ee40b087b91af797f381db7da10
Michael Fares <fares_michael@live.com>
2020-12-20 14:51:27 +0200

    Edit mod4_solution

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -43,7 +43,6 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // 'speak' method.
 // See Lecture 50, part 1
 for (var i = 0; i < names.length; i++) {
-  helloSpeaker.speak(names[i]);
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
